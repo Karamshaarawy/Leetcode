92c172928983124389a3c1fc0a32ed14f1659362
Karam Shaarawy <karam.shaarawy@gmail.com>
2024-08-03 04:28:02 +0300

    [1460] [Time Beats: 99.44%] [Memory Beats: 32.78%] - LeetPush

--- DCP-08-24/1460-Make-Two-Arrays-Equal-by-Reversing-Subarrays.js
+++ DCP-08-24/1460-Make-Two-Arrays-Equal-by-Reversing-Subarrays.js
@@ -6,12 +6,12 @@
 var canBeEqual = function(target, arr) {
     arrSorted=arr.sort((a,b)=>a-b)
     targetSorted=target.sort((a,b)=>a-b)
-    const result=targetSorted.map((element,index)=>{
+    let result=true
+    targetSorted.map((element,index)=>{
         if (arr[index]===element){
-            return true
         }else{
-            return false
+            result= false
         }
 })
-return !result.includes(false)
+return result
     }
