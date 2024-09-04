c371f86f481b358a585991922ec47b8c8042c130
Karam Shaarawy <karam.shaarawy@gmail.com>
2024-09-04 23:26:09 +0300

    [125] [Time Beats: 43.39%] [Memory Beats: 81.94%] - LeetPush

--- 125-Valid-Palindrome.js
+++ 125-Valid-Palindrome.js
@@ -3,16 +3,25 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    
     s=s.replace(/[!"#$%&'()*+,-./:;<=>?@[\\\]^_ `{|}~]/g, '').toLowerCase()
-    let reverse=new Array(s.length)
-    for (let char=reverse.length;char>0;char--){
-        console.log(char-1)
-        reverse[char-1]=s[(s.length-1)-(char-1)]
+    let left=0
+    let right=s.length-1
+    // let reverse=new Array(s.length)
+    // for (let char=reverse.length;char>0;char--){
+    //     console.log(char-1)
+    //     reverse[char-1]=s[(s.length-1)-(char-1)]
+    // }
+    // if(reverse.join("")===s){
+    //     return true
+    // }else{
+    //     return false
+    // }
+    while(left<right){
+        if (s[left]!==s[right]) return false
+        right--
+        left++
     }
-    if(reverse.join("")===s){
-        return true
-    }else{
-        return false
-    }
+    return true
 
 };
