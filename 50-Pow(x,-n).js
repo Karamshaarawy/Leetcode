69a2d7588719d881bbf8f2e393803e6273d1a8ab
Karam Shaarawy <karam.shaarawy@gmail.com>
2024-09-06 04:45:40 +0300

    [50] [Time Beats: 30.12%] [Memory Beats: 95.10%] - LeetPush

--- 50-Pow(x,-n).js
+++ 50-Pow(x,-n).js
@@ -4,19 +4,10 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-    if(n<0){
-        x=1/x
-        n=-n
-    }
-    function calculate(num) {
-        if (num === 0) return 1;
-        let half = calculate(Math.floor(num / 2));
-        if (num % 2 === 0) {
-            return half * half;
-        } else {
-            return half * half * x;
-        }
-    }
-    return calculate(n);
+    
+        if (n === 0) return 1;
+  if (n < 0) return 1 / myPow(x, -n);
+  if (n % 2 === 0) return myPow(x * x, n / 2);
+  return x * myPow(x, n - 1)
 };
 
